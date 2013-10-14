36a06f2e7dc7b9192a96f915dc332579d9fb0a2c
dzenanr <dzenanr@gmail.com>
2013-10-14 14:20:33 -0400

    add published balance

--- spirals/s01/web/bank_account.dart
+++ spirals/s01/web/bank_account.dart
@@ -4,17 +4,19 @@ import 'package:polymer/polymer.dart';
 @CustomTag('bank-account')
 class BankAccount extends PolymerElement {
   @published var bac;
+  @published double balance;
   double amount = 0.0;
   
-  created() {
+  inserted() {
     super.created();
-    // onPropertyChange here
+    balance = bac.balance;
   }
 
   transact(Event e, var detail, Node target) {
     InputElement amountInput = shadowRoot.query("#amount");
     if (!checkAmount(amountInput.value)) return;
     bac.transact(amount);
+    balance = bac.balance;
   }
 
   checkAmount(String in_amount) {
